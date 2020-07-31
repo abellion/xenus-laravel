6a37bce9e62d45ad37ac0489609a274e894ffd24
Antoine Bellion <antoinebellion@msn.com>
2020-07-31 17:19:40 +0200

    Add setup database trait

---
+++ tests/Support/SetupDatabase.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Xenus\Laravel\Tests\Support;
+
+use Xenus\Connection;
+
+trait SetupDatabase
+{
+    private $connection;
+
+    /**
+     * Delete the database
+     *
+     * @return void
+     */
+    private function deleteDatabase()
+    {
+        $this->connection->getDatabase()->drop();
+    }
+
+    /**
+     * Create the database
+     *
+     * @return void
+     */
+    private function createDatabase()
+    {
+        $this->connection = new Connection(getenv('MONGODB_URI'), getenv('MONGODB_DATABASE'));
+    }
+}
