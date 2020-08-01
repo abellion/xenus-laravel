65d22502c61d88f9270770ec52fe999f4a3e4a34
Antoine Bellion <antoinebellion@msn.com>
2020-08-01 13:16:48 +0200

    Add tests

---
+++ tests/Tests/Bridge/FailedJobsProviderTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Xenus\Laravel\Tests\Tests\Bridge;
+
+use Xenus\Laravel\Models\FailedJobs;
+use Xenus\Laravel\Bridge\FailedJobsProvider;
+use Xenus\Laravel\Tests\Support\SetupFailedJobsTest;
+
+use MongoDB\BSON\ObjectID;
+
+class FailedJobsProviderTest extends \PHPUnit\Framework\TestCase
+{
+    use SetupFailedJobsTest;
+
+    public function test_failed_jobs_are_correctly_flushed()
+    {
+        $provider = new FailedJobsProvider(
+            $collection = new FailedJobs($this->connection)
+        );
+
+        $collection->insertOne([
+            '_id' => $id = new ObjectID()
+        ]);
+
+        $this->assertEquals(
+            1, $collection->count()
+        );
+
+        $provider->flush();
+
+        $this->assertEquals(
+            0, $collection->count()
+        );
+    }
+
+    public function test_failed_jobs_are_correctly_forgotten()
+    {
+        $provider = new FailedJobsProvider(
+            $collection = new FailedJobs($this->connection)
+        );
+
+        $collection->insertOne([
+            '_id' => $id = new ObjectID()
+        ]);
+
+        $this->assertEquals(
+            1, $collection->count()
+        );
+
+        $provider->forget(
+            new ObjectID()
+        );
+
+        $this->assertEquals(
+            1, $collection->count()
+        );
+
+        $provider->forget(
+            $id
+        );
+
+        $this->assertEquals(
+            0, $collection->count()
+        );
+    }
+
+    public function test_all_failed_jobs_are_correctly_retrieved()
+    {
+        $provider = new FailedJobsProvider(
+            $collection = new FailedJobs($this->connection)
+        );
+
+        $collection->insertOne([
+            '_id' => $id = new ObjectID()
+        ]);
+
+        $items = $provider->all();
+
+        $this->assertIsArray(
+            $items
+        );
+
+        $this->assertCount(
+            1, $items
+        );
+    }
+
+    public function test_failed_jobs_are_correctly_found()
+    {
+        $provider = new FailedJobsProvider(
+            $collection = new FailedJobs($this->connection)
+        );
+
+        $collection->insertOne([
+            '_id' => $id = new ObjectID()
+        ]);
+
+        $this->assertNotNull(
+            $provider->find((string) $id)
+        );
+
+        $this->assertNull(
+            $provider->find((string) new ObjectID())
+        );
+    }
+}
