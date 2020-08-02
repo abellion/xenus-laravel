c9b466cf0c3aa1a4288cfd6d0f3138c1860748a4
Antoine Bellion <antoinebellion@msn.com>
2020-08-02 13:28:19 +0200

    Setup migrations bridge

---
+++ src/Bridge/Migrations.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Xenus\Laravel\Bridge;
+
+use Illuminate\Database\Migrations\MigrationRepositoryInterface;
+
+class Migrations implements MigrationRepositoryInterface
+{
+    // ...
+}
