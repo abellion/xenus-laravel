4536c8072ca49356b4a8a3b59d4580ce55cfca5c
Antoine Bellion <antoinebellion@msn.com>
2020-08-03 14:30:22 +0200

    Add xenus service provider

---
+++ src/XenusServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Xenus\Laravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class XenusServiceProvider extends ServiceProvider
+{
+    /**
+     * The collections to register
+     * @var array
+     */
+    protected $collections = [];
+
+    /**
+     * Register all the Xenus services
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->register(
+            new ConnectionServiceProvider($this->app)
+        );
+
+        $this->app->register(
+            new FailedJobsServiceProvider($this->app)
+        );
+
+        $this->app->register(
+            new MigrationsServiceProvider($this->app)
+        );
+
+        $this->app->register(
+            new CollectionsServiceProvider($this->app)->setCollections($this->collections)
+        );
+    }
+}
